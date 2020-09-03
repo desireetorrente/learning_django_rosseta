14e54d942725055223efa9a80d7b4bd25bac0d6b
Desi <dt@botxo.co>
2020-09-03 17:56:11 +0200

    importar modelo admin

--- learningrosetta/traducciones/admin.py
+++ learningrosetta/traducciones/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import Traducciones
+from .models import Traduccion
 
 # Register your models here.
 
-admin.site.register(Traducciones)
+admin.site.register(Traduccion)
